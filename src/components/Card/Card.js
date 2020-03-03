ed7c585d3030cb33ec64ce6d13de20e1462973f2
irinainina <irbris@mail.ru>
2020-03-03 07:50:20 +0200

    feat: add cards translation

--- src/components/Card/Card.js
+++ src/components/Card/Card.js
@@ -4,15 +4,34 @@ import cardsData from '../../data/cards';
 
 
 class Card extends Component {
+  flip = (event) => {
+    event.target.parentElement.classList.add('translate');
+  }
+  
+  flop = (event) => {
+    event.target.firstElementChild.classList.remove('translate');
+  }
+  
   renderCards = (arr, onCardClick, onBtnClick) => {
     return arr.map((item, index) => {
-      const { word, image } = item;    
+      const { word, image, translation } = item;    
       return (      
-        <div className="card"
-           style={{backgroundImage: `url(${image})`}}
-            key={index} 
-            onClick={(event) => onCardClick(event, index)} >
-          <div className="card-header">{word}</div>
+        <div className="card-container" 
+             onMouseLeave={(event) => this.flop(event)}
+             key={index}>
+          <div className="card">
+            <div className="front"
+                 style={{backgroundImage: `url(${image})`}} 
+                 onClick={(event) => onCardClick(event, index)}>
+              <div className="card-header">{word}</div>
+            </div>
+            <div className="back"
+                 style={{backgroundImage: `url(${image})`}}>             
+              <div className="card-header">{translation}</div>
+            </div>
+            <div className="rotate"
+                 onClick={(event) => this.flip(event)}></div>
+          </div>
         </div>
       );
     });
@@ -24,10 +43,10 @@ class Card extends Component {
       return (
        <div className="container">
         <div className="rating"></div>
-        {cards}
-                <div className="btns">      
-          <button className={play ? "btn repeat" : "btn"}
-                  onClick={onBtnClick}>
+          {cards}
+          <div className="btns">      
+            <button className={play ? "btn repeat" : "btn"}
+                    onClick={onBtnClick}>
            Play
           </button>
         </div>
