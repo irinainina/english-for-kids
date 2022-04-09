963828d081d7ec9c1285d050aa7047c1fb8c9623
irinainina <irbris@mail.ru>
2022-04-09 14:04:59 +0300

    feat: add deploy

---
+++ precache-manifest.b993abad4a05b87fbff19e0789cb6c72.js
@@ -0,0 +1,574 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8604ded05fbcaefbbbc1e4135664ce7f",
+    "url": "/english-for-kids/index.html"
+  },
+  {
+    "revision": "7bf4604a3da3099805e8",
+    "url": "/english-for-kids/static/css/main.704907d1.chunk.css"
+  },
+  {
+    "revision": "d7cf39d985e8565d67db",
+    "url": "/english-for-kids/static/js/2.815cce90.chunk.js"
+  },
+  {
+    "revision": "928d7b5eb39d16fe9a880722c974b51e",
+    "url": "/english-for-kids/static/js/2.815cce90.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7bf4604a3da3099805e8",
+    "url": "/english-for-kids/static/js/main.51ea6c65.chunk.js"
+  },
+  {
+    "revision": "0d6fc8351970eefa88ff",
+    "url": "/english-for-kids/static/js/runtime-main.d2653291.js"
+  },
+  {
+    "revision": "66b1074a0addfb2b98bd2036a481981e",
+    "url": "/english-for-kids/static/media/angry.66b1074a.jpg"
+  },
+  {
+    "revision": "f0418522e25cedf7b86a08f0895380c2",
+    "url": "/english-for-kids/static/media/angry.f0418522.mp3"
+  },
+  {
+    "revision": "06b9a73a022fa2936b1034dc1693ac78",
+    "url": "/english-for-kids/static/media/argue.06b9a73a.jpg"
+  },
+  {
+    "revision": "2de4cfacd6313ac860917d76304de584",
+    "url": "/english-for-kids/static/media/argue.2de4cfac.mp3"
+  },
+  {
+    "revision": "153666584d753d76875c51ef7d061f80",
+    "url": "/english-for-kids/static/media/big.15366658.jpg"
+  },
+  {
+    "revision": "cb6c6d8e805a88c0f7f2a812c0935ec3",
+    "url": "/english-for-kids/static/media/big.cb6c6d8e.mp3"
+  },
+  {
+    "revision": "44d2efb7a2c9fe3266585e2c9b7011da",
+    "url": "/english-for-kids/static/media/bird.44d2efb7.mp3"
+  },
+  {
+    "revision": "f0a80efa9f9add0d70970f5a77581524",
+    "url": "/english-for-kids/static/media/bird.f0a80efa.jpg"
+  },
+  {
+    "revision": "0a5a2e55b4ec0af9525a55e05199c602",
+    "url": "/english-for-kids/static/media/blouse.0a5a2e55.mp3"
+  },
+  {
+    "revision": "37a6d8fa2fe4863bca4c3e225758f11d",
+    "url": "/english-for-kids/static/media/blouse.37a6d8fa.jpg"
+  },
+  {
+    "revision": "1a1ae11acbc86ab96bb8418a835cd54a",
+    "url": "/english-for-kids/static/media/boot.1a1ae11a.jpg"
+  },
+  {
+    "revision": "ef6a341416eb8f4699004dcde54b569d",
+    "url": "/english-for-kids/static/media/boot.ef6a3414.mp3"
+  },
+  {
+    "revision": "0ddc52ca5fa2c97a5d2af93e72d57fda",
+    "url": "/english-for-kids/static/media/build.0ddc52ca.jpg"
+  },
+  {
+    "revision": "afd1a4dfceeaca69a276a6eb2bede2f7",
+    "url": "/english-for-kids/static/media/build.afd1a4df.mp3"
+  },
+  {
+    "revision": "4e29617e00c7bb0c7d2ac2566351c58b",
+    "url": "/english-for-kids/static/media/carry.4e29617e.jpg"
+  },
+  {
+    "revision": "75f0e19abd40d3ceea44e7391997e9e8",
+    "url": "/english-for-kids/static/media/carry.75f0e19a.mp3"
+  },
+  {
+    "revision": "48d4962f2c3af55db3a390eb3ec06953",
+    "url": "/english-for-kids/static/media/cat.48d4962f.mp3"
+  },
+  {
+    "revision": "e91220bbc24163608d4ec66973ab9794",
+    "url": "/english-for-kids/static/media/cat.e91220bb.jpg"
+  },
+  {
+    "revision": "02ee03911842c6129a683f306fc00573",
+    "url": "/english-for-kids/static/media/catch.02ee0391.jpg"
+  },
+  {
+    "revision": "c3306fa3934b20828871e1f4deff411e",
+    "url": "/english-for-kids/static/media/catch.c3306fa3.mp3"
+  },
+  {
+    "revision": "11bf9adb97cc44ee1e6ed8c4c33bc562",
+    "url": "/english-for-kids/static/media/chick.11bf9adb.jpg"
+  },
+  {
+    "revision": "e5ec9fdd6931d426afd5fbde329fdae0",
+    "url": "/english-for-kids/static/media/chick.e5ec9fdd.mp3"
+  },
+  {
+    "revision": "915430dfc507d2d4bcee12918b2cf386",
+    "url": "/english-for-kids/static/media/chicken.915430df.jpg"
+  },
+  {
+    "revision": "dde5fddc1a4b8ba1a1c9c266a67150b1",
+    "url": "/english-for-kids/static/media/chicken.dde5fddc.mp3"
+  },
+  {
+    "revision": "34fa79956300f3d5a47a03e9910f1c0a",
+    "url": "/english-for-kids/static/media/coat.34fa7995.mp3"
+  },
+  {
+    "revision": "8017b6cadcf926ecc263d6d535308f18",
+    "url": "/english-for-kids/static/media/coat.8017b6ca.jpg"
+  },
+  {
+    "revision": "8e3d6124f258a1690567f02ade38cd2d",
+    "url": "/english-for-kids/static/media/correct.8e3d6124.mp3"
+  },
+  {
+    "revision": "26a84a2cfd5fcc7ba6a1cd62c0302b86",
+    "url": "/english-for-kids/static/media/cry.26a84a2c.jpg"
+  },
+  {
+    "revision": "4844a20e76867c3014eece8a673172c3",
+    "url": "/english-for-kids/static/media/cry.4844a20e.mp3"
+  },
+  {
+    "revision": "4ac6b5030f67a8bf1e55c11aaa27fe7e",
+    "url": "/english-for-kids/static/media/dance.4ac6b503.mp3"
+  },
+  {
+    "revision": "c70cc103413c96ba60ca67ba80c9887b",
+    "url": "/english-for-kids/static/media/dance.c70cc103.jpg"
+  },
+  {
+    "revision": "380cae89e60617957ee8fa83eb311c60",
+    "url": "/english-for-kids/static/media/dive.380cae89.mp3"
+  },
+  {
+    "revision": "41b7d5099d937f93dfe62b6fac15e0f4",
+    "url": "/english-for-kids/static/media/dive.41b7d509.jpg"
+  },
+  {
+    "revision": "109811ba987c95eac202b1895b02c548",
+    "url": "/english-for-kids/static/media/dog.109811ba.jpg"
+  },
+  {
+    "revision": "72a09feddf540ea88698ca2414872d00",
+    "url": "/english-for-kids/static/media/dog.72a09fed.mp3"
+  },
+  {
+    "revision": "905447d119c58786d90d7b0b273ffc92",
+    "url": "/english-for-kids/static/media/dolphin.905447d1.jpg"
+  },
+  {
+    "revision": "a2a07c1baa7a4c62dc4b853914e0b23b",
+    "url": "/english-for-kids/static/media/dolphin.a2a07c1b.mp3"
+  },
+  {
+    "revision": "82be966527f22ece6d0777177c1086af",
+    "url": "/english-for-kids/static/media/draw.82be9665.jpg"
+  },
+  {
+    "revision": "9f97fc97c43970dc9bb4b9d794e6f11f",
+    "url": "/english-for-kids/static/media/draw.9f97fc97.mp3"
+  },
+  {
+    "revision": "264b02bf777b825ec1c3a2c0a6cfe995",
+    "url": "/english-for-kids/static/media/dress.264b02bf.jpg"
+  },
+  {
+    "revision": "96f2733bfcc9a5b3b5d678e0794773d3",
+    "url": "/english-for-kids/static/media/dress.96f2733b.mp3"
+  },
+  {
+    "revision": "257aed67be0d34797ef5fd1deb1a5e83",
+    "url": "/english-for-kids/static/media/drive.257aed67.mp3"
+  },
+  {
+    "revision": "7756db52707d2246d00d76f3441f0461",
+    "url": "/english-for-kids/static/media/drive.7756db52.jpg"
+  },
+  {
+    "revision": "8cb9f43a7de74e9c1ad6b450a174d419",
+    "url": "/english-for-kids/static/media/drop.8cb9f43a.jpg"
+  },
+  {
+    "revision": "975b1eb1b01cc5a1616d54487c52c614",
+    "url": "/english-for-kids/static/media/drop.975b1eb1.mp3"
+  },
+  {
+    "revision": "32fc22b2ceef21006a7f31e18e44f1f3",
+    "url": "/english-for-kids/static/media/error.32fc22b2.mp3"
+  },
+  {
+    "revision": "18423bbf3f91d01afc65e4c858760423",
+    "url": "/english-for-kids/static/media/failure.18423bbf.mp3"
+  },
+  {
+    "revision": "ef048b74a95bb383a32b338b7efbff5f",
+    "url": "/english-for-kids/static/media/failure.ef048b74.jpg"
+  },
+  {
+    "revision": "2d5d071eeb19a6c9d241b6396b57df8d",
+    "url": "/english-for-kids/static/media/fast.2d5d071e.jpg"
+  },
+  {
+    "revision": "96cdd22e8a7272aa1474d619c8355271",
+    "url": "/english-for-kids/static/media/fast.96cdd22e.mp3"
+  },
+  {
+    "revision": "27eb7746974fb185c3ba988a35dec6a2",
+    "url": "/english-for-kids/static/media/fish.27eb7746.jpg"
+  },
+  {
+    "revision": "d901a31675e5b27dcadd79656693ca14",
+    "url": "/english-for-kids/static/media/fish.d901a316.mp3"
+  },
+  {
+    "revision": "296cfcd4453a5096dd23818bcf7b91de",
+    "url": "/english-for-kids/static/media/fish1.296cfcd4.jpg"
+  },
+  {
+    "revision": "6122fa03d35911d60060b20e4cc3b750",
+    "url": "/english-for-kids/static/media/fly.6122fa03.jpg"
+  },
+  {
+    "revision": "90b6cc3f6e0c7132de2cbd7036dcf43f",
+    "url": "/english-for-kids/static/media/fly.90b6cc3f.mp3"
+  },
+  {
+    "revision": "cc4d120d6b00f372c564897087039e44",
+    "url": "/english-for-kids/static/media/friendly.cc4d120d.mp3"
+  },
+  {
+    "revision": "ebfd8e9a1c053e195d1f9f2e11acac64",
+    "url": "/english-for-kids/static/media/friendly.ebfd8e9a.jpg"
+  },
+  {
+    "revision": "2cc171b2f40c938d189948ba4753a0f0",
+    "url": "/english-for-kids/static/media/frog.2cc171b2.mp3"
+  },
+  {
+    "revision": "e8a07dca26fab55d97e331ef200198b7",
+    "url": "/english-for-kids/static/media/frog.e8a07dca.jpg"
+  },
+  {
+    "revision": "061acd829105d34e85ddc8ab3e5df29d",
+    "url": "/english-for-kids/static/media/giraffe.061acd82.mp3"
+  },
+  {
+    "revision": "32ab22c7ecb96be3712250d393f70be4",
+    "url": "/english-for-kids/static/media/giraffe.32ab22c7.jpg"
+  },
+  {
+    "revision": "430d399a7ce87903d5f2864ae0d7f5a4",
+    "url": "/english-for-kids/static/media/happy.430d399a.mp3"
+  },
+  {
+    "revision": "996126dde90d360fba342a6ed869ddcb",
+    "url": "/english-for-kids/static/media/happy.996126dd.jpg"
+  },
+  {
+    "revision": "8a825b3b2ed78ae6e832d6c5e6eb4bf8",
+    "url": "/english-for-kids/static/media/horse.8a825b3b.jpg"
+  },
+  {
+    "revision": "9d0ec1e7c037fae40dc1dbff024f1bfb",
+    "url": "/english-for-kids/static/media/horse.9d0ec1e7.mp3"
+  },
+  {
+    "revision": "23b5891d4813d7e42f856b1604a7de56",
+    "url": "/english-for-kids/static/media/hug.23b5891d.mp3"
+  },
+  {
+    "revision": "9dda3c46797d000746726bf13bde563f",
+    "url": "/english-for-kids/static/media/hug.9dda3c46.jpg"
+  },
+  {
+    "revision": "6afc98481f8d340991f27dd2328e6df1",
+    "url": "/english-for-kids/static/media/jump.6afc9848.mp3"
+  },
+  {
+    "revision": "ac945820596adfc1f97e1c069dac3021",
+    "url": "/english-for-kids/static/media/jump.ac945820.jpg"
+  },
+  {
+    "revision": "1705ee3c94d3fc2447409f7b3bc9bacc",
+    "url": "/english-for-kids/static/media/laugh.1705ee3c.mp3"
+  },
+  {
+    "revision": "8bbca0b9b6185c2cfd444b9fb02d9a6a",
+    "url": "/english-for-kids/static/media/laugh.8bbca0b9.jpg"
+  },
+  {
+    "revision": "273c0650db0c6eed70269b57db9b81cf",
+    "url": "/english-for-kids/static/media/lion.273c0650.jpg"
+  },
+  {
+    "revision": "5ac914680eafcf80bb3cef52a37bfc00",
+    "url": "/english-for-kids/static/media/lion.5ac91468.mp3"
+  },
+  {
+    "revision": "5c89651ca40d71b966531856d5dfbd80",
+    "url": "/english-for-kids/static/media/mouse.5c89651c.mp3"
+  },
+  {
+    "revision": "a73259d9b4e2f26e5d4cfabffe5035ec",
+    "url": "/english-for-kids/static/media/mouse.a73259d9.jpg"
+  },
+  {
+    "revision": "76174203b2b1c072b091ad4af36120a0",
+    "url": "/english-for-kids/static/media/old.76174203.jpg"
+  },
+  {
+    "revision": "7cdf49852b48bbde31fdcb387c9ed4af",
+    "url": "/english-for-kids/static/media/old.7cdf4985.mp3"
+  },
+  {
+    "revision": "191b88af2397fea1e03bff7d0da0eaea",
+    "url": "/english-for-kids/static/media/open.191b88af.jpg"
+  },
+  {
+    "revision": "a40d30e5db45b9e17a7ed875ecd22500",
+    "url": "/english-for-kids/static/media/open.a40d30e5.mp3"
+  },
+  {
+    "revision": "197f10696d74dd686dcc86c2a8a514bd",
+    "url": "/english-for-kids/static/media/pants.197f1069.jpg"
+  },
+  {
+    "revision": "869f17e9837c8ee91dbea4e0d08a5744",
+    "url": "/english-for-kids/static/media/pants.869f17e9.mp3"
+  },
+  {
+    "revision": "72810927770d837ca615b30a40a297c6",
+    "url": "/english-for-kids/static/media/pig.72810927.jpg"
+  },
+  {
+    "revision": "beebe8c7804c55b7f79eeaf7f821a9c2",
+    "url": "/english-for-kids/static/media/pig.beebe8c7.mp3"
+  },
+  {
+    "revision": "75cbc10cbb26297f71178d391cc2afd6",
+    "url": "/english-for-kids/static/media/play.75cbc10c.jpg"
+  },
+  {
+    "revision": "88677512e598ff18a1e57bba412acc18",
+    "url": "/english-for-kids/static/media/play.88677512.mp3"
+  },
+  {
+    "revision": "676a0aad2e15b32a6f3b5030a720024e",
+    "url": "/english-for-kids/static/media/point.676a0aad.mp3"
+  },
+  {
+    "revision": "ba50996aff17808a69d34861a7c19e08",
+    "url": "/english-for-kids/static/media/point.ba50996a.jpg"
+  },
+  {
+    "revision": "504fb7ebb45ba0008e7a771d9156e563",
+    "url": "/english-for-kids/static/media/pull.504fb7eb.mp3"
+  },
+  {
+    "revision": "958b53a6b019607b07c5bf3f4f20d32d",
+    "url": "/english-for-kids/static/media/pull.958b53a6.jpg"
+  },
+  {
+    "revision": "7bc5a0861f4a31511152948a8261cb39",
+    "url": "/english-for-kids/static/media/push.7bc5a086.mp3"
+  },
+  {
+    "revision": "f518349168cd921b95791f204e4a79a9",
+    "url": "/english-for-kids/static/media/push.f5183491.jpg"
+  },
+  {
+    "revision": "7242c16a20a5be1a12d3195157590ae6",
+    "url": "/english-for-kids/static/media/rabbit.7242c16a.jpg"
+  },
+  {
+    "revision": "c64513230d51aaff8bd3e4af0c541651",
+    "url": "/english-for-kids/static/media/rabbit.c6451323.mp3"
+  },
+  {
+    "revision": "d0f11196ad6869c6fcecdb938fcb4d2c",
+    "url": "/english-for-kids/static/media/repeat.d0f11196.svg"
+  },
+  {
+    "revision": "7f6cc09762fd1237122ac3c01a09dbc2",
+    "url": "/english-for-kids/static/media/ride.7f6cc097.mp3"
+  },
+  {
+    "revision": "b7a774748627512134820b0d486506b2",
+    "url": "/english-for-kids/static/media/ride.b7a77474.jpg"
+  },
+  {
+    "revision": "aaa793ab8b2189f22a46190d6be3f1d0",
+    "url": "/english-for-kids/static/media/rotate.aaa793ab.svg"
+  },
+  {
+    "revision": "0e8d157f4693dc11064e05ac2f277bc2",
+    "url": "/english-for-kids/static/media/run.0e8d157f.jpg"
+  },
+  {
+    "revision": "64fd8c4408301348cfb79559f50d02f1",
+    "url": "/english-for-kids/static/media/run.64fd8c44.mp3"
+  },
+  {
+    "revision": "5942f97941613a551f34d518004d70e0",
+    "url": "/english-for-kids/static/media/sad.5942f979.jpg"
+  },
+  {
+    "revision": "f4fed6e9ae2429db9c5797c90e295ff7",
+    "url": "/english-for-kids/static/media/sad.f4fed6e9.mp3"
+  },
+  {
+    "revision": "201dedac9802ff3f4a55498d960a2677",
+    "url": "/english-for-kids/static/media/scared.201dedac.mp3"
+  },
+  {
+    "revision": "e2747b49d4f4006c48b513e1f91dd6be",
+    "url": "/english-for-kids/static/media/scared.e2747b49.jpg"
+  },
+  {
+    "revision": "858deffb5d1aea4adb1cba88c4ad8274",
+    "url": "/english-for-kids/static/media/sheep.858deffb.mp3"
+  },
+  {
+    "revision": "8d2deb43b2dce27648f240bb8a26409d",
+    "url": "/english-for-kids/static/media/sheep.8d2deb43.jpg"
+  },
+  {
+    "revision": "3bed14734cec424f1882327d6a170e23",
+    "url": "/english-for-kids/static/media/shirt.3bed1473.jpg"
+  },
+  {
+    "revision": "e04713a11296956c4afcc4f087884383",
+    "url": "/english-for-kids/static/media/shirt.e04713a1.mp3"
+  },
+  {
+    "revision": "5522fbfd15188dff04e6fda22ab306ae",
+    "url": "/english-for-kids/static/media/shoe.5522fbfd.jpg"
+  },
+  {
+    "revision": "686de2693041fb70147af3fbbb600245",
+    "url": "/english-for-kids/static/media/shoe.686de269.mp3"
+  },
+  {
+    "revision": "568ddebaacfbcba07796c36381ce1802",
+    "url": "/english-for-kids/static/media/sing.568ddeba.mp3"
+  },
+  {
+    "revision": "d2d5278629ce55144bc300a6c868d00b",
+    "url": "/english-for-kids/static/media/sing.d2d52786.jpg"
+  },
+  {
+    "revision": "081bf61e056754a4659d872807db122c",
+    "url": "/english-for-kids/static/media/skip.081bf61e.jpg"
+  },
+  {
+    "revision": "095ffccf75be7c06b492475fc27fe7ca",
+    "url": "/english-for-kids/static/media/skip.095ffccf.mp3"
+  },
+  {
+    "revision": "b0ee2c918e4df26b125a8845c8a2e87b",
+    "url": "/english-for-kids/static/media/skirt.b0ee2c91.jpg"
+  },
+  {
+    "revision": "b1549e9a1c04323374cf81a8eb60c133",
+    "url": "/english-for-kids/static/media/skirt.b1549e9a.mp3"
+  },
+  {
+    "revision": "7bbcaa169436574711e08d9042a0a33c",
+    "url": "/english-for-kids/static/media/slow.7bbcaa16.jpg"
+  },
+  {
+    "revision": "9f330077901eb5680f8ab7885daf5a46",
+    "url": "/english-for-kids/static/media/slow.9f330077.mp3"
+  },
+  {
+    "revision": "a9f7a59dd503560b4d78bff6e5de4165",
+    "url": "/english-for-kids/static/media/small.a9f7a59d.jpg"
+  },
+  {
+    "revision": "f3424fe2cd6115f25c66f992fa59ac61",
+    "url": "/english-for-kids/static/media/small.f3424fe2.mp3"
+  },
+  {
+    "revision": "54cb842c0383edb589ca98d03fb4fcfb",
+    "url": "/english-for-kids/static/media/smile.54cb842c.mp3"
+  },
+  {
+    "revision": "f55cb4706f1b57dddbf0091a1e200c64",
+    "url": "/english-for-kids/static/media/smile.f55cb470.jpg"
+  },
+  {
+    "revision": "43e453a65c5299f98ee9153511fea504",
+    "url": "/english-for-kids/static/media/star-win.43e453a6.svg"
+  },
+  {
+    "revision": "dc1ba88811653762f2c5c10ab67b69ce",
+    "url": "/english-for-kids/static/media/star.dc1ba888.svg"
+  },
+  {
+    "revision": "3a0796260ee7756307a1f3be5fe59b4a",
+    "url": "/english-for-kids/static/media/success.3a079626.jpg"
+  },
+  {
+    "revision": "beda7e18ed738e1ce4d01e0d38d795af",
+    "url": "/english-for-kids/static/media/success.beda7e18.mp3"
+  },
+  {
+    "revision": "0667ed30cdd1c94aa449408907a7a97b",
+    "url": "/english-for-kids/static/media/surprised.0667ed30.jpg"
+  },
+  {
+    "revision": "5b5478c6b24542f6bcec69c80eecf62e",
+    "url": "/english-for-kids/static/media/surprised.5b5478c6.mp3"
+  },
+  {
+    "revision": "bddf06874af486bf43eac9ef1acb38ea",
+    "url": "/english-for-kids/static/media/swim.bddf0687.jpg"
+  },
+  {
+    "revision": "f271ef3147bf760ad8b5dcc7cbf30df9",
+    "url": "/english-for-kids/static/media/swim.f271ef31.mp3"
+  },
+  {
+    "revision": "73533205128fa9734e3d9dd99f0214a6",
+    "url": "/english-for-kids/static/media/tired.73533205.mp3"
+  },
+  {
+    "revision": "a169662a32b03725663b918c5c86cbff",
+    "url": "/english-for-kids/static/media/tired.a169662a.jpg"
+  },
+  {
+    "revision": "55abff197db6d9dc716d34d28cfb52c0",
+    "url": "/english-for-kids/static/media/turtle.55abff19.mp3"
+  },
+  {
+    "revision": "b1e5849e6d2e703ce073f72658e58370",
+    "url": "/english-for-kids/static/media/turtle.b1e5849e.jpg"
+  },
+  {
+    "revision": "9bb158bdd56e44123ba618244f6f91c8",
+    "url": "/english-for-kids/static/media/unfriendly.9bb158bd.jpg"
+  },
+  {
+    "revision": "dee93a5655d9807d3155f03a19b30d64",
+    "url": "/english-for-kids/static/media/unfriendly.dee93a56.mp3"
+  },
+  {
+    "revision": "7498933a0ee12a09df5a23bd42367c5a",
+    "url": "/english-for-kids/static/media/young.7498933a.jpg"
+  },
+  {
+    "revision": "7cf71a224b685cfe01b4d5c3e075878d",
+    "url": "/english-for-kids/static/media/young.7cf71a22.mp3"
+  }
+]);
--- src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './components/App/App';
-import { HashRouter } from 'react-router-dom';
-
-ReactDOM.render(<HashRouter><App /></HashRouter>, document.getElementById('root'));
